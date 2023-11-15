8909c3b06814f5eb7fe40bdbf0d7489eb48ef91e
Neil Quirk <73691309+Neil-Quirk@users.noreply.github.com>
2023-11-15 21:35:29 +0200

    Initial Commit
    Full commit of the Drag and Drop Kanban board, built with Angular

---
+++ kanban/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainviewComponent } from './pages/mainview/mainview.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainviewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
